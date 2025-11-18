6cce5a2bbfc0a8bf64c04a7299115df145739b4b
twolenss <133645933+twolenss@users.noreply.github.com>
2025-11-18 22:16:47 +0800

    Add files via upload

--- subject-code.ts
+++ subject-code.ts
@@ -2,28 +2,58 @@ interface SubjectCode {
     codeNo: string;
     course: string;
     year: string | number;
-    dept : string
+    dept: string;
 }
+
 export interface SubjectGroup {
     subjectId: string;
     subjectTitle: string;
+    units?: number;
     codes: SubjectCode[];
 }   
 
 export interface DepartmentGroup {
-  dept: string;
-  deptCode: string;
-  loadingDepartments: string[];
+    dept: string;
+    deptCode: string;
+    loadingDepartments: string[];
 }
 
-export interface Exam {
-  code: string;
-  subjectId: string;
-  title: string;
-  course: string;
-  dept: string;
-  day: string;
-  time: string;
-  room: string;
-  year: string;
+export interface ProgramGroup {
+    program: string;
+    year: any | number;
+    subjects: {
+        set: number;
+        subjectId: string;
+        subjectTitle: string;
+        units?: number; 
+        codeNo: string;
+    }[];
+}
+
+export interface ProgramSchedule {
+    program: string;
+    year: number;
+    dept: string;
+    subjects: { 
+        subjectId: string; 
+        subjectTitle: string; 
+        codeNo: string;
+        units?: number; 
+    }[];
+    schedule: { [slot: string]: string };
+    remainingSubjects?: number;
+}
+
+export interface Rooms {
+    roomNumber: string;
+    schedule: {
+        subjectId: string;
+        codeNo: string;
+        course: string;
+        yearLevel: number;
+        dept: string;
+        day: string;
+        time: string;
+        units?: number; 
+    }[];
 }
